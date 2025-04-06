7e51a966d416307f0056bb16741db1914567ca6f
Wojciech <wojtek.larecki@gmail.com>
2025-04-06 22:48:11 +0200

    Create ui views.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,27 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //ui->stackedWidget->
+    ui->accountsTableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->manageAccountsTableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    //connect(ui->pushButton_2, &QPushButton::clicked, this, &MainWindow::onButton3Cliecked);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+// --------EVENTS---------
+void MainWindow::onButton3Cliecked() {
+//     //auto isVisible = ui->pushButton_3->isVisible();
+//     //ui->pushButton_3->setVisible(!isVisible);
+//     //QMessageBox::information(this, "Clicked", "push_button3 was clicked!");
+//     //ui->stackedWidget->setCurrentIndex(1);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -17,6 +17,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void onButton3Cliecked();
+
 private:
     Ui::MainWindow *ui;
 };
